4e9673178bf05366d05b440ffda1e77d625aea60
Louise <louise@hven.local>
2019-02-25 07:16:15 -0800

    done

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Louise on 2/25/19.
+//  Copyright © 2019 louise. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
